77e60ff71e7507130c969048cc6207a0f4123765
Guillaume Bielli <gbielli@Guillaumes-MacBook-Pro.local>
2024-05-15 18:02:12 +0200

    blog

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,4 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+const strapiBaseUri = process.env.API_URL || "http://127.0.0.1:1337";
+
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  modules: [
+    '@nuxtjs/strapi',
+  ],
+  strapi: {   
+    url: strapiBaseUri,
+    prefix: '/api',  
+    version: 'v4',
+    cookie: {},
+    cookieName: 'strapi_jwt',
+  },
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 })
